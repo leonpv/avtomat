db1ab4ff745456c48d55bf568492fe864ad24b2f
Sacred <mafiadon00@yandex.ru>
2018-03-25 21:13:04 +0300

    Англофикация

--- index.php
+++ index.php
@@ -45,8 +45,8 @@ var_dump($_SESSION['username']);
         }
         else{
             echo "
-            <li><a href='edit_profile.php'>Редактировать профиль</a></li>
-            <li><a href='Scripts/exit.php'>Выход</a></li>
+            <li><a href='edit_profile.php'>Edit profile</a></li>
+            <li><a href='Scripts/exit.php'>Exit</a></li>
             ";
         }
         ?>
