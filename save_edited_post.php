a03c3b7d8835bda4bd59435239051afa66eee37e
Sacred <mafiadon00@yandex.ru>
2018-03-28 19:59:25 +0300

    commit

--- save_edited_post.php
+++ save_edited_post.php
@@ -1,7 +1,7 @@
 <?php
-include "../inc/config.php";
+include "inc/config.php";
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $con -> query("UPDATE Job SET Name = '{$_POST['Name']}', Location = '{$_POST['Location']}', Type = '{$_POST['Type']}',
- Category = '{$_POST['Category']}', Description = '{$_POST['Description']}' WHERE id = '{2}'");
+ Category = '{$_POST['Category']}', Description = '{$_POST['Description']}' WHERE id = '2'");
     header('Location: ../index.php');
 }
